0d1376d18c75f1d9852c2960a97e2fa1f9029199
DankChristmas <134347527+DankChristmas@users.noreply.github.com>
2025-04-13 16:40:53 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleContent(element) {
-    const content = element.nextElementSibling;
-    if (content.classList.contains('show')) {
-        content.classList.remove('show');
-    } else {
-        content.classList.add('show');
-    }
-}
